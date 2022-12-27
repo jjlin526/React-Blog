6a235e8e1aff85aeed9c7dd6ed1d13f5c765a08b
jjlin526 <jjlin526@gmail.com>
2022-12-26 21:59:58 -0500

    Used the event object in click event handler

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,19 @@
 const Home = () => {
-  const handleClick = () => {
-    console.log("button clicked");
+  const handleClick = (e) => {
+    console.log("button clicked", e);
   };
 
-  const handleClickAgain = (name) => {
-    console.log("hello " + name);
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={() => handleClickAgain("mario")}>Click me again</button>
+      <button onClick={(e) => handleClickAgain("mario", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
