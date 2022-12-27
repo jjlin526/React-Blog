91c1d939c94b9ff26456b8bd6e47022cc98e549e
jjlin526 <jjlin526@gmail.com>
2022-12-26 21:11:31 -0500

    Add Navbar to App component

---
+++ dojo-blog/src/Navbar.js
@@ -0,0 +1,14 @@
+// generate stateless functional component using Simple React Component
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+
+export default Navbar;
