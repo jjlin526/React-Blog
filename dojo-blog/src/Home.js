45f1d2b9ff38ad4253d8dfb28405aa075c8d2b20
Jonathan <114364831+jjlin526@users.noreply.github.com>
2022-12-27 14:01:30 -0500

    Fix blog rendering of null data before fetch

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,12 +4,6 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  // runs initially and whenever there is a re-render - good for fetching data
   useEffect(() => {
     // GET request which returns a promise
     fetch('http://localhost:8000/blogs')
@@ -19,18 +13,12 @@ const Home = () => {
     .then((data) => {
         setBlogs(data)
     })
-    // add dependency array
+    // add dependency array, fetch only occurs on load
   }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="Mario's Blogs"
-        handleDelete={handleDelete}
-      />
-      <button onClick={() => setName('luigi')}>Change Name</button>
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
