6691ad36991eaf99bedb2103185e2466b9b401ea
Jonathan <114364831+jjlin526@users.noreply.github.com>
2022-12-27 14:23:18 -0500

    Add conditional loading message for blog list

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,21 +3,28 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    // GET request which returns a promise
-    fetch('http://localhost:8000/blogs')
-    .then(res => {
-        return res.json()
-    })
-    .then((data) => {
-        setBlogs(data)
-    })
+    // Simulate fetch time
+    setTimeout(() => {
+      // GET request which returns a promise
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
+
     // add dependency array, fetch only occurs on load
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
