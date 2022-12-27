9e8694df580cf2695ff89095fc5e7131ec0cfab3
jjlin526 <jjlin526@gmail.com>
2022-12-26 21:58:15 -0500

    Add click event handlers wrapped in anonymous functions to prevent accidental invokation

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,9 +1,19 @@
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
-    );
-}
+  const handleClick = () => {
+    console.log("button clicked");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log("hello " + name);
+  };
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("mario")}>Click me again</button>
+    </div>
+  );
+};
 
 export default Home;
