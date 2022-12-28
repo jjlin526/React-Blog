3846182c11a42c29b899ea0553bdd011e37ec00f
Jonathan <114364831+jjlin526@users.noreply.github.com>
2022-12-28 13:35:51 -0500

    Use useHistory hook to redirect user to home page after blog is created

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,11 +1,12 @@
-import { exactProp } from "@mui/utils";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     // prevent refresh
@@ -23,6 +24,7 @@ const Create = () => {
     }).then(() => {
       console.log("new blog added");
       setIsPending(false);
+      history.push("/");
     });
   };
 
